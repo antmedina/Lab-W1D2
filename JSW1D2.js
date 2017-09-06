0b7aa007dea6ef51097cfd096f01ea9a83728857
antmedina <ant.n.medina@gmail.com>
2017-09-06 18:59:11 -0400

    need to fix second loop

--- JSW1D2.js
+++ JSW1D2.js
@@ -1,81 +1,43 @@
 var friends = ["Rick","Kenneth","Sebastian","Razvan","Matt"]
-
+var len = friends.length;
+var i =0;
 var a=3
 var b=(a-1)
-while(a>=2){  
-     console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[0]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
+
+friends.forEach (function(len){
+while(a>=3){  
+     console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[i]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
     a--;
     b--;
 }
 do{
-(a+ " lines of code in the file, "+a+ " lines of code; "+friends[0]+" strikes one out, clears it all out," +b+" lines of code in the file!");
+console.log(a+ " lines of code in the file, "+a+ " lines of code; "+friends[i]+" strikes one out, clears it all out, " +b+" line of code in the file!");
 a--;
 b--;
-}
-while(a===2);
+}while(a===2);
 do{
-    (a+ " line of code in the file, "+a+ " line of code; "+friends[0]+" strikes one out, clears it all out, no more lines of code in the file!");
-    a--;
-    }
-    while(a=1);
-
+   console.log (a+ " line of code in the file, "+a+ " line of code; "+friends[i]+" strikes one out, clears it all out, no more lines of code in the file!");
+a--;    
+}
+    while(a===1);
+    i++;
+} )
 console.log("")
 
-// var a=99
-// var b=(a-1)
-// while(a>=1){  
-//      console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[1]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
-//     a--;
-//     b--;
+// while(a>=3){  
+//     console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[0]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
+//    a--;
+//    b--;
 // }
 // do{
-// (a+ " line of code in the file, "+a+ " line of code; "+friends[1]+" strikes one out, clears it all out, no more lines of code in the file!");
+// console.log(a+ " lines of code in the file, "+a+ " lines of code; "+friends[0]+" strikes one out, clears it all out, " +b+" line of code in the file!");
 // a--;
-// }
-// while(a===1);
-
-// console.log("")
-
-// var a=99
-// var b=(a-1)
-// while(a>=1){  
-//      console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[2]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
-//     a--;
-//     b--;
-// }
+// b--;
+// }while(a===2);
 // do{
-// (a+ " line of code in the file, "+a+ " line of code; "+friends[2]+" strikes one out, clears it all out, no more lines of code in the file!");
-// a--;
+//   console.log (a+ " line of code in the file, "+a+ " line of code; "+friends[0]+" strikes one out, clears it all out, no more lines of code in the file!");
+// a--;    
 // }
-// while(a===1);
+//    while(a===1);
 
-// console.log("")
-
-// var a=99
-// var b=(a-1)
-// while(a>=1){  
-//      console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[3]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
-//     a--;
-//     b--;
-// }
-// do{
-// (a+ " line of code in the file, "+a+ " line of code; "+friends[3]+" strikes one out, clears it all out, no more lines of code in the file!");
-// a--;
-// }
-// while(a===1);
-
-// console.log("")
-
-// var a=99
-// var b=(a-1)
-// while(a>=1){  
-//      console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[4]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
-//     a--;
-//     b--;
-// }
-// do{
-// (a+ " line of code in the file, "+a+ " line of code; "+friends[4]+" strikes one out, clears it all out, no more lines of code in the file!");
-// a--;
-// }
-// while(a===1);
 // console.log("")
