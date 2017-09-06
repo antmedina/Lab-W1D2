c031ed5d262e7865b02d078bc992553e2cd9f34e
antmedina <ant.n.medina@gmail.com>
2017-09-06 15:56:29 -0400

    edits pt.1

--- JSW1D2.js
+++ JSW1D2.js
@@ -1,75 +1,81 @@
 var friends = ["Rick","Kenneth","Sebastian","Razvan","Matt"]
 
-var a=99
+var a=3
 var b=(a-1)
-while(a>=1){  
+while(a>=2){  
      console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[0]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
     a--;
     b--;
 }
 do{
-(a+ " line of code in the file, "+a+ " line of code; "+friends[0]+" strikes one out, clears it all out, no more lines of code in the file!");
+(a+ " lines of code in the file, "+a+ " lines of code; "+friends[0]+" strikes one out, clears it all out," +b+" lines of code in the file!");
 a--;
+b--;
 }
-while(a===1);
-
-console.log("")
-
-var a=99
-var b=(a-1)
-while(a>=1){  
-     console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[1]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
-    a--;
-    b--;
-}
+while(a===2);
 do{
-(a+ " line of code in the file, "+a+ " line of code; "+friends[1]+" strikes one out, clears it all out, no more lines of code in the file!");
-a--;
-}
-while(a===1);
-
-console.log("")
-
-var a=99
-var b=(a-1)
-while(a>=1){  
-     console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[2]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
+    (a+ " line of code in the file, "+a+ " line of code; "+friends[0]+" strikes one out, clears it all out, no more lines of code in the file!");
     a--;
-    b--;
-}
-do{
-(a+ " line of code in the file, "+a+ " line of code; "+friends[2]+" strikes one out, clears it all out, no more lines of code in the file!");
-a--;
-}
-while(a===1);
+    }
+    while(a=1);
 
 console.log("")
 
-var a=99
-var b=(a-1)
-while(a>=1){  
-     console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[3]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
-    a--;
-    b--;
-}
-do{
-(a+ " line of code in the file, "+a+ " line of code; "+friends[3]+" strikes one out, clears it all out, no more lines of code in the file!");
-a--;
-}
-while(a===1);
+// var a=99
+// var b=(a-1)
+// while(a>=1){  
+//      console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[1]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
+//     a--;
+//     b--;
+// }
+// do{
+// (a+ " line of code in the file, "+a+ " line of code; "+friends[1]+" strikes one out, clears it all out, no more lines of code in the file!");
+// a--;
+// }
+// while(a===1);
 
-console.log("")
+// console.log("")
 
-var a=99
-var b=(a-1)
-while(a>=1){  
-     console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[4]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
-    a--;
-    b--;
-}
-do{
-(a+ " line of code in the file, "+a+ " line of code; "+friends[4]+" strikes one out, clears it all out, no more lines of code in the file!");
-a--;
-}
-while(a===1);
-console.log("")
+// var a=99
+// var b=(a-1)
+// while(a>=1){  
+//      console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[2]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
+//     a--;
+//     b--;
+// }
+// do{
+// (a+ " line of code in the file, "+a+ " line of code; "+friends[2]+" strikes one out, clears it all out, no more lines of code in the file!");
+// a--;
+// }
+// while(a===1);
+
+// console.log("")
+
+// var a=99
+// var b=(a-1)
+// while(a>=1){  
+//      console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[3]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
+//     a--;
+//     b--;
+// }
+// do{
+// (a+ " line of code in the file, "+a+ " line of code; "+friends[3]+" strikes one out, clears it all out, no more lines of code in the file!");
+// a--;
+// }
+// while(a===1);
+
+// console.log("")
+
+// var a=99
+// var b=(a-1)
+// while(a>=1){  
+//      console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[4]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
+//     a--;
+//     b--;
+// }
+// do{
+// (a+ " line of code in the file, "+a+ " line of code; "+friends[4]+" strikes one out, clears it all out, no more lines of code in the file!");
+// a--;
+// }
+// while(a===1);
+// console.log("")
