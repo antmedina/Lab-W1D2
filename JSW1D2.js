de631801ff9558e00c6d55dd0d1be04a116b6d38
antmedina <ant.n.medina@gmail.com>
2017-09-07 09:47:39 -0400

    Trimmed code after seeeing answer: + 6 lines of code.

--- JSW1D2.js
+++ JSW1D2.js
@@ -1,19 +1,14 @@
 var friends = ["Rick","Kenneth","Sebastian","Razvan","Matt"]
-var len = friends.length;
 var a=99
-var b=(a-1)
 
-for (var i=0; i<len;i++ ) {
+for (var i=0; i<friends.length; i++ ) {
 while(a>=3){  
-     console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[i]+ " strikes one out, clears it all out, "+b+" lines of code in the file!");
+     console.log(a+" lines of code in the file, "+a+" lines of code; "+friends[i]+ " strikes one out, clears it all out, "+(a-1)+" lines of code in the file!");
     a--;
-    b--;
 }
-
 do{
-console.log(a+ " lines of code in the file, "+a+ " lines of code; "+friends[i]+" strikes one out, clears it all out, " +b+" line of code in the file!");
+console.log(a+ " lines of code in the file, "+a+ " lines of code; "+friends[i]+" strikes one out, clears it all out, " +(a-1)+" line of code in the file!");
 a--;
-b--;
 }while(a===2);
 do{
    console.log (a+ " line of code in the file, "+a+ " line of code; "+friends[i]+" strikes one out, clears it all out, no more lines of code in the file!");
@@ -22,5 +17,4 @@ a--;
     while(a===1);
     console.log("");   
     a=99;
-    b=a-1;
 }
